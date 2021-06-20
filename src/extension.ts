15a103d7942f12374ae83dcece8d3c019ea05b63
Dian <guodian44@gmail.com>
2021-06-20 16:53:58 -0400

    add basic timer template

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,123 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let myStatusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
+    // console.log('Congratulations, your extension "helloworld" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        'helloworld.helloWorld',
-        () => {
-            // The code you place here will be executed every time your command is executed
-            // Display a message box to the user
-            vscode.window.showInformationMessage('Hello chat from HelloWorld!');
-        }
+    let timer = 0;
+    let isTimerRunning = false;
+
+    myStatusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        100
     );
-
-    context.subscriptions.push(disposable); // gets return value from dispo
+    myStatusBarItem.text = 'some timer text';
+    myStatusBarItem.show();
+    // myStatusBarItem.
+    context.subscriptions.push(myStatusBarItem);
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('helloworld.askQuestion', async () => {
-            const answer = await vscode.window.showInformationMessage(
-                'How was your day?',
-                'good',
-                'bad'
-            );
+        vscode.commands.registerCommand('helloworld.startTimer', async () => {
+            isTimerRunning = true;
 
-            if (answer === 'bad') {
-                vscode.window.showInformationMessage('sorry to hear that');
-            } else {
-                console.log(answer);
+            if (isTimerRunning) {
+                var asdf = setInterval(function () {
+                    timer += 1;
+                    myStatusBarItem.text = `timer: ${timer}`;
+                }, 1000);
             }
         })
     );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('helloworld.stopTimer', async () => {
+            clearInterval(asdf);
+        })
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('helloworld.resetTimer', async () => {})
+    );
+
+    // let disposable = vscode.commands.registerCommand(
+    //     'helloworld.helloWorld',
+    //     () => {
+    //         vscode.window.showInformationMessage('Hello chat from HelloWorld!');
+    //     }
+    // );
+
+    // context.subscriptions.push(disposable); // gets return value from dispo
+
+    // context.subscriptions.push(
+    //     vscode.commands.registerCommand('helloworld.askQuestion', async () => {
+    //         const answer = await vscode.window.showInformationMessage(
+    //             'How was your day?',
+    //             'good',
+    //             'bad'
+    //         );
+
+    //         if (answer === 'bad') {
+    //             vscode.window.showInformationMessage('sorry to hear that');
+    //         } else {
+    //             console.log(answer);
+    //         }
+    //     })
+    // );
+
+    // register a command that is invoked when the status bar
+    // item is selected
+    // const myCommandId = 'helloworld.showSelectionCount';
+    // context.subscriptions.push(
+    //     vscode.commands.registerCommand(myCommandId, () => {
+    //         const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+    //         vscode.window.showInformationMessage(
+    //             `Yeah, ${n} line(s) selected... Keep going!`
+    //         );
+    //     })
+    // );
+
+    // // create a new status bar item that we can now manage
+    // myStatusBarItem = vscode.window.createStatusBarItem(
+    //     vscode.StatusBarAlignment.Right,
+    //     100
+    // );
+    // myStatusBarItem.command = myCommandId;
+    // context.subscriptions.push(myStatusBarItem);
+
+    // // register some listener that make sure the status bar
+    // // item always up-to-date
+    // context.subscriptions.push(
+    //     vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
+    // );
+    // context.subscriptions.push(
+    //     vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
+    // );
+
+    // // update status bar item once at start
+    // updateStatusBarItem();
 }
 
-// this method is called when your extension is deactivated
+// todo keep track of timer var here
 export function deactivate() {}
+
+// function updateStatusBarItem(): void {
+//     const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+//     if (n > 0) {
+//         myStatusBarItem.text = `asdfasdfasdfas ${n} line(s) selected`;
+//         myStatusBarItem.show();
+//     } else {
+//         myStatusBarItem.hide();
+//     }
+// }
+
+// function getNumberOfSelectedLines(
+//     editor: vscode.TextEditor | undefined
+// ): number {
+//     let lines = 0;
+//     if (editor) {
+//         lines = editor.selections.reduce(
+//             (prev, curr) => prev + (curr.end.line - curr.start.line),
+//             0
+//         );
+//     }
+//     return lines;
+// }
